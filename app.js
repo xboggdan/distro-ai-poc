1e2d15d36f2fb769762e85b39a2e57b17ee7daac
xboggdan <xboggdan@gmail.com>
2025-12-11 22:57:18 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// Inside App.js
+import CoverArtWizard from './components/CoverArtWizard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Cover Art Tool</h1>
+      <CoverArtWizard />
+    </div>
+  );
+}
